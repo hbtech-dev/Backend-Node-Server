c7fdf9961f2ea54e6456a8388f1de2154bd081e7
Bilal <livebilal321@gmail.com>
2026-09-18 19:14:18 +0500

    Support configurable premium vs economy delivery service for international shipments

--- src/services/dhl.service.js
+++ src/services/dhl.service.js
@@ -167,7 +167,7 @@ exports.createDHLShipment = async ({ sender = {}, recipient = {}, orderNum = '',
             ...(activeProduct === 'V53WPAK' ? {
               services: {
                 endorsement: 'RETURN',
-                premium: true
+                premium: userDhlConfig.isPremium !== undefined ? Boolean(userDhlConfig.isPremium) : true
               }
             } : {})
           }
